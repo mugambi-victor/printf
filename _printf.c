81288e5001bb52b706845e55a2a39e94523e0cc6
mugambi-victor <victormmugambi@gmail.com>
2023-09-02 19:54:04 +0300

    betty _printf

--- _printf.c
+++ _printf.c
@@ -4,55 +4,53 @@
 
 int _printf(const char *format, ...)
 {
-     
 	va_list args;
-    int count;
-    va_start(args, format);
-     if (format == NULL)
-    {
-        return -1;
-    }
-    
+	int count;
 
-    count = 0;
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	count = 0;
 
-    while (*format != '\0')
-    {
-         if (*format == '%')
-        {
-            format++;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
 
-	    if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                while (*str != '\0')
-                {
-                    putchar(*str);
-                    count++;
-                    str++;
-                }
-            }
-            else if (*format == 'c')
-            {
-               char ch = (char)va_arg(args, int);
-                _putchar(ch);
-                count++;
-            }
-            else if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-        }
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					count++;
+					str++;
+				}
+			}
+			else if (*format == 'c')
+			{
+				char ch = (char)va_arg(args, int);
 
-        format++;
-    }
+				_putchar(ch);
+				count++;
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
 
-    va_end(args);
+	return (count);
 
-    return count;
 }
